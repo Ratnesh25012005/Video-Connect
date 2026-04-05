8a539acc59bc71fa04187b1ccc9f921ac7a843ac
Ratnesh Singh Bhandari <ratneshsinghbhandari25@gmail.com>
2026-04-05 16:47:31 +0530

    added dotenv file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {createServer} from "node:http";
 import {Server} from "socket.io";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import connectToSocket from "./controllers/socketManager.js";
 import userRoutes from "./routes/users.routes.js"
 
-
+dotenv.config();
 
 
 const app=express();
@@ -19,11 +20,12 @@ app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
 
-app.use("/api/v1/users",userRoutes);
 
+app.use("/api/v1/users",userRoutes);
+const url=process.env.MONGODB_URL;
 
 const start = async () =>{
-    const connectionDB = await mongoose.connect("mongodb+srv://singh12bhandari_db_user:9u1F8jbHJQ10UW7J@cluster0.ihmc245.mongodb.net/")
+    const connectionDB = await mongoose.connect(url)
 
     console.log(`Mongo Connect DB Host:${connectionDB.connection.host}`)
     server.listen(app.get("port"), () => {
